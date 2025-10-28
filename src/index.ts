72fe9ee4675a2b6ce0499578c9ff0eddd43e77c5
alifarukm <alifaruk.km01@gmail.com>
2025-10-28 23:34:45 +0300

    remove un used ep

--- src/index.ts
+++ src/index.ts
@@ -13,11 +13,4 @@ app.get('/', (c) => {
 })
 
 
-app.get('/', async (c) => {
-  const prisma = await prismaClients.fetch(c.env.DB)
-  const users = await prisma.user.findMany()
-  console.log('users', users)
-  return c.json(users)
-})
-
 export default app
